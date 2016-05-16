3f451e39d47b8789f4069e114e3f435423146c25
Bjorn Hansen <holomorph@gmail.com>
2016-05-15 22:42:36 -0700

    player list

---
+++ roster-redux/app/components/Player-list.jsx
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Player from './Player';
+
+
+const PlayerList = ({ players }) => (
+  <div>
+    <h2>Players:</h2>
+    <ul>
+      {players.map( p => p.name)}
+    </ul>
+  </div>
+);
+
+PlayerList.propTypes = {
+  players: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  debugger;
+  return { players: state.present.players };
+};
+
+export default connect(mapStateToProps)(PlayerList)
