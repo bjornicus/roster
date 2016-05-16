744138d83976dc95197144e4dd66a67b2dca2cbe
Bjorn Hansen <holomorph@gmail.com>
2016-05-15 23:19:44 -0700

    reducers!

--- roster-redux/app/reducers.js
+++ roster-redux/app/reducers.js
@@ -1,22 +1,36 @@
 let nextId = 0;
 
-function playerReducer(state, action) {
+function playerReducer(state, action) { 
     switch (actio.type) {
         case 'ADD_PLAYER':
             return {id: nextId++, name: action.playerName, isPlaying: false};
         case 'SUB_PLAYER': 
+           if (action.playerId !== state.id){
+                return state;
+            }
             return { ...state, isPlaying: !state.isPlaying };
         default:
             return state
     }
 }
 
+function playersReducer(state, action) {
+    switch (action.type) {
+        case 'ADD_PLAYER':
+          return [
+            ...state,
+            playerReducer(undefined, action)
+          ]
+        case 'SUB_PLAYER':
+          return state.map(p =>
+            playerReducer(p, action)
+          )
+        default:
+          return state
+      }
+}
+
 
 export default (state = { players : []}, action) => {
-  switch (action.type) {
-    case 'SUB':
-      return state; //{...state, {players: state.players.concat([{name: 'steve'}])}};
-    default:
-      return state
-  }
+    return {...state, players: playersReducer(state.players, action)}
 }
