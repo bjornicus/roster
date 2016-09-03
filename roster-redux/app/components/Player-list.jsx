4dba08f31d8c2ff888f142829fdc5f51d97a7115
Bjorn Hansen <holomorph@gmail.com>
2016-09-03 12:09:11 -0700

    player list headings are labels

--- roster-redux/app/components/Player-list.jsx
+++ roster-redux/app/components/Player-list.jsx
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Player from './Player';
+import { Label } from 'react-bootstrap';
 
 
 const PlayerList = ({ heading, players, playerFilter, onSubClick}) => (
   <div>
-    <h2>{heading}</h2>
+    <h2><Label>{heading}</Label></h2>
     <ul className='list-group'>
       {players.filter(playerFilter).map( p => <li className='list-group-item' key={p.id}><Player player={p} onSubClick={onSubClick} /></li> )}
     </ul>
