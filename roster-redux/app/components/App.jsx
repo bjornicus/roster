e990a27e3c7275996cfced936a6b5fe2d86b8ea5
Bjorn Hansen <holomorph@gmail.com>
2016-09-03 12:30:34 -0700

    undo in top right, h* margin-top smaller

--- roster-redux/app/components/App.jsx
+++ roster-redux/app/components/App.jsx
@@ -3,17 +3,24 @@ import PlayerList from './Player-list'
 import AddPlayer from './Add-player'
 import Undo from './Undo'
 import GameClock from './Game-clock'
+import { Grid, Row, Col } from 'react-bootstrap';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div id="content">
-        <GameClock />
-        <Undo />
-        <AddPlayer />
-        <PlayerList heading="Playing" playerFilter={ p => p.isPlaying }/>
-        <PlayerList heading="Substitutes" playerFilter={ p => !p.isPlaying }/>
-      </div>
+      <Grid id="content">
+        <Row>
+          <Col xs={10}><GameClock /></Col>
+          <Col xs={2}><Undo /></Col>
+        </Row>
+        <Row>
+          <AddPlayer />
+        </Row>
+        <Row>
+          <PlayerList heading="Playing" playerFilter={ p => p.isPlaying }/>
+          <PlayerList heading="Substitutes" playerFilter={ p => !p.isPlaying }/>
+        </Row>
+      </Grid>
     );
   }
 }
