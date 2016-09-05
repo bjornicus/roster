ce14fe80593b65fba5135efdcef3bd22882163d6
Bjorn Hansen <holomorph@gmail.com>
2016-09-05 12:25:35 -0700

    sort players by last sub time'

--- roster-redux/app/components/Player-list.jsx
+++ roster-redux/app/components/Player-list.jsx
@@ -8,7 +8,11 @@ const PlayerList = ({ heading, players, playerFilter}) => (
   <div>
     <h3><Label>{heading}</Label></h3>
     <ul className='list-group'>
-      {players.filter(playerFilter).map( p => <li className='list-group-item' key={p.id}><Player player={p} /></li> )}
+      {players
+        .filter(playerFilter)
+        .sort((p1,p2) => Math.max(p1.subInTime, p1.subOutTime) - Math.max(p2.subInTime, p2.subOutTime))
+        .map( p => <li className='list-group-item' key={p.id}><Player player={p} /></li> )
+      }
     </ul>
   </div>
 );
