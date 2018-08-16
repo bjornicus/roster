44d57485b19e5dce117ea2edfa60b26ba55e4578
Bjorn Hansen <bh@hoylu.com>
2018-08-15 22:10:16 -0700

    move fair-play to root of repo & nuke the old stuff

--- react-roster/src/components/Time-display.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Label } from 'react-bootstrap';
-import formatTime from '../time-format';
-
-export default ({ time, clockStyle }) => {
-  return (
-      <Label className={'clock ' + clockStyle}>{formatTime(time)}</Label>
-  );
-}
