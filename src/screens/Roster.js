ff118c8adfbe67ee26415bf3eac821411535e3a1
Bjorn Hansen <bh@hoylu.com>
2018-08-16 14:12:35 -0700

    scroll the roster if it doesn't fit

--- src/screens/Roster.js
+++ src/screens/Roster.js
@@ -2,16 +2,21 @@ import React from 'react';
 import AddPlayer from '../components/Add-player';
 import Screen from './Screen';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+const Players = styled.ul`
+  overflow: scroll;
+`;
 
 export function Roster({ players }) {
   return (
     <Screen>
       <AddPlayer />
-      <ul>
+      <Players>
         {players.map(p => (
           <li key={p.id}>{p.name}</li>
         ))}
-      </ul>
+      </Players>
     </Screen>
   );
 }
