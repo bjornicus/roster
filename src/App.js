c0336803f090e90e69f7d84ca2a55b6f1d8b3303
Bjorn Hansen <bh@hoylu.com>
2018-08-16 13:38:50 -0700

    react-swipeable-veiws instead of react router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
 import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router';
-import { MemoryRouter as Router } from 'react-router-dom';
 import Game from './components/Game';
 import Stats from './components/Stats';
 import Roster from './components/Roster';
 import Swipeable from 'react-swipeable';
+import SwipeableViews from 'react-swipeable-views';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages: ['/roster', '/game', '/stats'],
+      currentPageIndex: 0
+    };
+  }
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route
-            path="/roster"
-            render={({ history }) => (
-              <Swipeable
-                onSwipedLeft={e => {
-                  history.replace('/game');
-                }}
-              >
-                <Roster />
-              </Swipeable>
-            )}
-          />
-          <Route
-            path="/game"
-            render={({ history }) => (
-              <Swipeable
-                onSwipedRight={e => {
-                  history.replace('/roster');
-                }}
-                onSwipedLeft={e => {
-                  history.replace('/stats');
-                }}
-              >
-                <Game />
-              </Swipeable>
-            )}
-          />
-          <Route
-            path="/stats"
-            render={({ history }) => (
-              <Swipeable
-                onSwipedRight={e => {
-                  history.replace('/game');
-                }}
-              >
-                <Stats />
-              </Swipeable>
-            )}
-          />
-          <Redirect to="/game" />
-        </Switch>
-      </Router>
+      <SwipeableViews>
+        <Roster />
+        <Game />
+        <Stats />
+      </SwipeableViews>
     );
   }
 }
