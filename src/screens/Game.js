999646c2a0dfad1237121136d2443f8d0070630c
Bjorn Hansen <bh@hoylu.com>
2018-08-16 21:24:58 -0700

    game screen header buttons at top

--- src/screens/Game.js
+++ src/screens/Game.js
@@ -3,12 +3,20 @@ import PlayerList from '../components/Player-list';
 import Undo from '../components/Undo';
 import GameClock from '../components/Game-clock';
 import Screen from './Screen';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export default function() {
   return (
     <Screen>
-      <GameClock />
-      <Undo />
+      <Header>
+        <GameClock />
+        <Undo />
+      </Header>
       <PlayerList heading="Playing" playerFilter={p => p.isPlaying} />
       <PlayerList heading="Substitutes" playerFilter={p => !p.isPlaying} />
     </Screen>
