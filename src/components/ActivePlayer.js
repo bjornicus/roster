58d6e45497993c536436dbf464c6feb1b5de8578
Bjorn Hansen <bh@hoylu.com>
2018-08-17 08:38:47 -0700

    remove uneede stuff from ActivePlayer

--- src/components/ActivePlayer.js
+++ src/components/ActivePlayer.js
@@ -5,17 +5,6 @@ import TimeDisplay from './Time-display';
 import { Button } from './Button';
 import { PlayerName, Player } from './PlayerList';
 
-function totalPlayingTime(player, currentTime) {
-  if (player.isPlaying) {
-    return player.previousPlaytime + (currentTime - player.subInTime);
-  }
-  return player.previousPlaytime;
-}
-
-// function totalPlayingPercent(player, currentTime) {
-//   return 100 * (totalPlayingTime(player, currentTime) / currentTime);
-// }
-
 function timeSinceLastSub(player, currentTime) {
   if (player.isPlaying) {
     return currentTime - player.subInTime;
@@ -23,47 +12,28 @@ function timeSinceLastSub(player, currentTime) {
   return currentTime - player.subOutTime;
 }
 
-const ActivePlayer = ({
-  player,
-  currentTime,
-  onSubClick,
-  onGoalScored,
-  onToggleClock
-}) => (
+const ActivePlayer = ({ player, currentTime, onSubClick }) => (
   <Player>
     <Button onClick={() => onSubClick(player.id)}>SUB</Button>
 
     <PlayerName> {player.name} </PlayerName>
-
-    {/* <Button onClick={() => onGoalScored(player.id)}> {player.goals} </Button> */}
-
-    {/* <ProgressBar
-          now={totalPlayingPercent(player, currentTime)}
-          onClick={() => onToggleClock(player.id)}
-        /> */}
     <div>
       <TimeDisplay
         clockStyle="last-sub"
         time={timeSinceLastSub(player, currentTime)}
       />
-      {/* <TimeDisplay
-        clockStyle="total-played"
-        time={totalPlayingTime(player, currentTime)}
-      /> */}
     </div>
   </Player>
 );
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = state => {
   return { currentTime: state.clock.currentTime };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     onSubClick: (playerId, currentTime) =>
-      dispatch({ type: 'SUB_PLAYER', playerId, currentTime }),
-    onGoalScored: playerId => dispatch({ type: 'GOAL', playerId }),
-    onToggleClock: playerId => dispatch({ type: 'TOGGLE_CLOCK', playerId })
+      dispatch({ type: 'SUB_PLAYER', playerId, currentTime })
   };
 };
 
@@ -71,8 +41,6 @@ function mergeProps(stateProps, dispatchProps, ownProps) {
   return Object.assign({}, ownProps, {
     onSubClick: playerId =>
       dispatchProps.onSubClick(playerId, stateProps.currentTime),
-    onGoalScored: playerId => dispatchProps.onGoalScored(playerId),
-    onToggleClock: playerId => dispatchProps.onToggleClock(playerId),
     currentTime: stateProps.currentTime
   });
 }
