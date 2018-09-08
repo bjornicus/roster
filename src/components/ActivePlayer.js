401f2fb58b218e78a1fcea55f4bfab2582551308
Bjorn Hansen <bh@hoylu.com>
2018-09-08 10:09:06 -0700

    progress bar for total playing time

--- src/components/ActivePlayer.js
+++ src/components/ActivePlayer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import { connect } from 'react-redux';
 import TimeDisplay from './Time-display';
 import { Button } from './Button';
--- src/components/PlayerStats.js
+++ src/components/PlayerStats.js
@@ -9,16 +9,39 @@ const GoalCounter = styled(Button)`
   background-color: darkorchid;
 `;
 
+const ProgressBar = styled.progress`
+  direction: rtl;
+  height: 20px;
+  margin-right: 0.3rem;
+  -webkit-appearance: none;
+  &::-webkit-progress-bar {
+    background-color: #eee;
+    border-radius: 2px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
+  }
+  &::-webkit-progress-value {
+    /* background-image: -webkit-linear-gradient(
+        -45deg,
+        transparent 33%,
+        rgba(0, 0, 0, 0.1) 33%,
+        rgba(0, 0, 0, 0.1) 66%,
+        transparent 66%
+      ),
+      -webkit-linear-gradient(top, rgba(255, 255, 255, 0.25), rgba(0, 0, 0, 0.25)),
+      -webkit-linear-gradient(left, #09c, #f44); */
+
+    background-color: darkorchid;
+    border-radius: 2px;
+    background-size: 35px 20px, 100% 100%, 100% 100%;
+  }
+`;
+
 export function totalPlayingTime(player, currentTime) {
   if (player.isPlaying) {
     return player.previousPlaytime + (currentTime - player.subInTime);
   }
   return player.previousPlaytime;
 }
-
-// function totalPlayingPercent(player, currentTime) {
-//   return 100 * (totalPlayingTime(player, currentTime) / currentTime);
-// }
 
 const PlayerStats = ({ player, currentTime, onGoalScored }) => (
   <Player>
@@ -28,10 +51,10 @@ const PlayerStats = ({ player, currentTime, onGoalScored }) => (
     </GoalCounter>
     <PlayerName> {player.name} </PlayerName>
 
-    {/* <ProgressBar
-          now={totalPlayingPercent(player, currentTime)}
-          onClick={() => onToggleClock(player.id)}
-        /> */}
+    <ProgressBar
+      max={Math.max(currentTime, 50 * 60)}
+      value={totalPlayingTime(player, currentTime)}
+    />
     <TimeDisplay
       clockStyle="total-played"
       time={totalPlayingTime(player, currentTime)}
